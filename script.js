1126c31394fce77c119d8e8d075f3d838e7a6f35
Noman Ali <noman710@protonmail.com>
2022-01-07 14:51:35 -0500

    Created ediv element with class row

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".add-rows").click(function (){
+        let select = document.querySelector(".grid-container")
+        let rowElement = document.createElement("div")
+        rowElement.classList.add("row")
+        console.log(rowElement)
+        
+
+    })
+})
