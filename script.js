0c2c14416edcbe7e94fdcdd1960568076ff39668
Noman Ali <noman710@protonmail.com>
2022-01-07 15:10:27 -0500

    Dropdown made, box color change on color done

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 $(document).ready(function () {
+    /*addRows user story
     $(".add-rows").click(function (){
         let select = document.querySelector(".grid-container")
         let rowElement = document.createElement("div")
         rowElement.classList.add("row")
         console.log(rowElement)
+        let box = document.getElementById("row-one")
+        console.log(box)
         
+        rowElement.appendChild(box)
+        rowElement.appendChild(box)
+        rowElement.appendChild(box)
+        console.log(rowElement)
 
+    })*/
+    //Dropdown made, function for clicking on a box to change color user story
+     $('.col').click(function () {
+        let c = document.querySelector("#colors")
+        $(this).css("background-color",c.value)
     })
 })
